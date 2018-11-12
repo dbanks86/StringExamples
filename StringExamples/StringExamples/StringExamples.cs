c4482e8411a485d69b157221b6ac5bf53a58080b
dbanks86 <dbanks86@gmail.com>
2018-11-11 19:59:25 -0500

    Add ReverseString() method

--- StringExamples/StringExamples/StringExamples.cs
+++ StringExamples/StringExamples/StringExamples.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StringExamples
+namespace StringUtility
 {
     public class StringExamples
     {
+        /// <summary>
+        /// Reverses text
+        /// </summary>
+        /// <param name="text">String to be reversed</param>
+        public string ReverseString(string text)
+        {
+            if (string.IsNullOrEmpty(text)) throw new InvalidOperationException();
+
+            var textAsCharArray = text.ToCharArray();
+
+            //loops iterations stop at half point of array since two values are being switched during each loop iteration
+            for (int i = 0; i < textAsCharArray.Length / 2; i++)
+            {
+                char firstValue = textAsCharArray[i];
+                char secondValue = textAsCharArray[textAsCharArray.Length - i - 1];
+
+                textAsCharArray[i] = secondValue;
+                textAsCharArray[text.Length - i - 1] = firstValue;
+            }
+
+            return new string(textAsCharArray);
+        }
     }
 }
